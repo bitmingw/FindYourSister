de1cf247c7ccfae9a36d29beddaa307ce8ed6797
Ming Wen <bitmingw@gmail.com>
2015-03-17 14:46:53 +0800

    general header file

---
+++ include/fys.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2015, Ming Wen
+//
+// fys.hpp
+//
+// General header file of this software.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+
+#include <unistd.h>
+
