8d45e6a66369c85da0dd9c9a3c923de3b942ad96
Ming Wen <bitmingw@gmail.com>
2015-04-14 10:27:18 +0800

    init features header

---
+++ include/features.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2015, Ming Wen
+
+#ifndef _H_FEATURES
+#define _H_FEATURES
+
+using namespace std;
+using namespace cv;
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+
+namespace fys {
+
+} // namespace fys
+
+#endif // _H_FEATURES
