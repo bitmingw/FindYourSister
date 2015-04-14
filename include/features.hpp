b6e9825f95a6e820df11afe2daa4fd2adec6ccee
Ming Wen <bitmingw@gmail.com>
2015-04-14 11:06:03 +0800

    init features structure

--- include/features.hpp
+++ include/features.hpp
@@ -6,11 +6,27 @@
 using namespace std;
 using namespace cv;
 
+#include "json_features.hpp"
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/nonfree/features2d.hpp>
 
 namespace fys {
+
+class Features
+{
+
+public:
+    Features(JsonFeatures config);
+    ~Features();
+
+private:
+    JsonFeatures config;
+    //cv::FeatureDetector& detector;
+    //cv::DescriptorExtractor& extractor;
+    //cv::DescriptorMatcher& matcher;
+};
 
 } // namespace fys
 
---
+++ src/features.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2015, Ming Wen
+
+#include "fys.hpp"
+#include "features.hpp"
+
+using namespace std;
+
+namespace fys {
+
+Features::Features(JsonFeatures config)
+    : config(config)
+{
+}
+
+} // namespace fys
+
