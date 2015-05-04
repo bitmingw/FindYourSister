15e159b90db3ef09d0f7ef688193186a23f70eac
Ming Wen <bitmingw@gmail.com>
2015-05-04 10:19:29 +0800

    setup algorithm interface

---
+++ include/fys_algorithms.hpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2015, Ming Wen
+
+#ifndef _H_FYS_ALGORITHMS
+#define _H_FYS_ALGORITHMS
+
+#include "fys.hpp"
+#include "json_features.hpp"
+
+using namespace std;
+using namespace cv;
+
+namespace fys {
+
+class FysFeatureDetector
+{
+public:
+    FysFeatureDetector(string name);
+    FysFeatureDetector(string name, vector<string> paras, vector<string> paraTypes);
+    ~FysFeatureDetector();
+    cv::FeatureDetector* getDetector();
+private:
+    cv::FeatureDetector* detector;
+};
+
+class FysDescriptorExtractor
+{
+public:
+    FysDescriptorExtractor(string name);
+    FysDescriptorExtractor(string name, vector<string> paras, vector<string> paraTypes);
+    ~FysDescriptorExtractor();
+    cv::DescriptorExtractor* getExtractor();
+private:
+    cv::DescriptorExtractor* extractor;
+}
+
+class FysDescriptorMatcher
+{
+public:
+    FysDescriptorMatcher(string name);
+    FysDescriptorMatcher(string name, vector<string> paras, vector<string> paraTypes);
+    ~FysDescriptorMatcher();
+    cv::DescriptorMatcher* getMatcher();
+private:
+    cv::DescriptorMatcher* matcher;
+}
+
+} // namespace fys
+
+#endif // _H_FYS_ALGORITHMS
+
